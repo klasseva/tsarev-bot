3701fc5360dce81dc1014bcce6f27fc268023490
klasseva <148004932+klasseva@users.noreply.github.com>
2026-05-28 07:52:29 +0500

    Update plus.py

--- cogs/plus.py
+++ cogs/plus.py
@@ -30,7 +30,6 @@ class PlusCog(commands.Cog):
         title: str,
         event: app_commands.Choice[str],
         slots: int = 0,
-        dop slotы: int = 0,
         date: str | None = None,
         role: discord.Role | None = None,
         branch: str | None = None,
